b5f9555f4c86b11e08a00b2e666d6234de930148
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2022-12-17 21:22:29 +0300

    Updates

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import dogsReducer from '../slices/dogsSlice';
-
-export default configureStore({
-  reducer: {
-    dogs: dogsReducer,
-  },
-});
--- src/utils/routes.js
+++
@@ -1,3 +0,0 @@
-const apiPath = 'http://shibe.online/api/shibes?count=6&urls=true&httpsUrls=true';
-
-export default apiPath;
